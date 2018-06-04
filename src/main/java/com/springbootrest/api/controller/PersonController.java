738b94bb199bd5a53d8eda8d8ad44e617cecb795
Sujan Shrestha <nepkoder@gmail.com>
2018-06-04 16:12:58 +0545

    review controller

--- src/main/java/com/springbootrest/api/controller/PersonController.java
+++ src/main/java/com/springbootrest/api/controller/PersonController.java
@@ -7,11 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,7 +23,7 @@ import com.springbootrest.api.service.PersonService;
 
 @RestController
 @RequestMapping("/api")
-// @CrossOrigin(origins="http://localhost:4200")
+ @CrossOrigin(origins="http://localhost:4200")
 public class PersonController {
 
 	// just for log
